0894c58934d95d518cc5f38500738d021dcfc2b2
Justine Fricou <justine.fricou@gmail.com>
2022-04-07 17:37:51 +0200

    Adding a README

--- ipythonKernel.py
+++ ipythonKernel.py
@@ -1,64 +1,18 @@
-import IPython
 from ipykernel.kernelapp import launch_new_instance
-from ipykernel.kernelbase import Kernel
-from ipykernel.kernelapp import IPKernelApp as IPKA
-from traitlets.config import Config
 
 def main():
-    # 1st try:
-    # Using the ipython_config and ipython_kernel_config files by setting
-    # c.InteractiveShellApp.exec_lines and c.TerminalIPythonApp.exec_lines
-    #launch_new_instance()
-
-    # 2nd try (successful):
-    # ~/.ipython/profile_default/startup/init_lsst.py will be
-    # executed during IPython setup
-    #launch_new_instance()
-
-    # 3rd try:
-    # https://ipython.readthedocs.io/en/7.x/api/generated/IPython.html?highlight=kernel#IPython.start_kernel
-    # The IPython user namespace will be initialized with these variables:
+    # The IPython kernel's user namespace will be initialized with the
+    # variables contained in this dictionary:
     namespace = dict(
         a = 0,
         b = 54
     )
     launch_new_instance(user_ns=namespace)
 
-    # 4th try:
-    """ namespace = dict(
-        a = 0,
-        b = 54
-    )
-    app = IPKA.instance()
-    app.initialize([])
-    app.kernel.user_ns = namespace
-    app.start() """
-
-    # 5th try:
-    # https://ipython.readthedocs.io/en/stable/config/intro.html#running-ipython-from-python
-    """ c = Config()
-    c.InteractiveShellApp.exec_lines = ["pppp = 555"]
-    c.InteractiveShellApp.init_code()
-    print(IPKA.connection_file)
-    IPython.start_kernel(config=c) """
-
-    # 6th try:
-    # https://ipython.readthedocs.io/en/7.x/config/intro.html#command-line-arguments
-
-    # 7th try:
-    # https://ipython.readthedocs.io/en/7.x/config/intro.html#the-ipython-directory
-    # Launching the launch_ipython_kernel script
-
-    # 8th try:
-    """ my_kernel = IPKA.instance()
-    my_kernel.initialize()
-    my_kernel.user_ns = dict(test5 = 5)
-    my_kernel.start() """
-
-    # 9th try:
-    """ test = 41
-    IPython.embed_kernel(local_ns=dict(test2 = 3, test3 = 4)) """
- 
+    # 2nd try (successful):
+    # ~/.ipython/profile_default/startup/init_lsst.py will be
+    # executed during IPython setup
+    #launch_new_instance() 
 
 if __name__ == "__main__" :
     main()
