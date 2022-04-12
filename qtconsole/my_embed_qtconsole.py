ecb485f335589e6ee94041285e84ef49f07fb1cc
Justine Fricou <justine.fricou@gmail.com>
2022-04-12 14:40:06 +0200

    Addinghard-coded configuration of the kernel connection file

--- qtconsole/my_embed_qtconsole.py
+++ qtconsole/my_embed_qtconsole.py
@@ -15,29 +15,31 @@ from qtconsole.client import QtKernelClient
 from jupyter_client import KernelClient
 from jupyter_client.utils import run_sync
 import nest_asyncio
+import jupyter_core
 
-
-nest_asyncio.apply()
+""" Modified code from the qtconsole repo : qtconsoleapp.py (new_frontend_connection function)
+and embed_qtconsole.py """
 
 # The ID of an installed kernel, e.g. 'bash' or 'ir'.
 USE_KERNEL = 'python3'
+
+nest_asyncio.apply()
 
 def make_jupyter_widget_with_kernel():
     """Start a kernel, connect to it, and create a RichJupyterWidget to use it
     """
 
-    # Using code in qtconsoleapp.py (new_frontend_connection function) and in
-    # embed_qtconsole.py
-    kernel_client = QtKernelClient()#connection_file=connection_file, config=self.config)
-    kernel_client.load_connection_file('/home/jfricou/.local/share/jupyter/runtime/kernel-' +
-        '32675' + '.json') # Opens the given connection file and stores it
+    kernel_client = QtKernelClient()
+    kernel_client.load_connection_file(jupyter_core.paths.jupyter_runtime_dir() +
+        '/kernel-' + '19983' + '.json')
     kernel_client.start_channels()
 
     # TODO: make this work
     # execute_interactive = run_sync(KernelClient._async_execute_interactive)
     # execute_interactive(KernelClient, "b")
 
-    # TODO: see what's in BaseFrontendMixin (base_frontend_mixin.py)
+    # TODO: see what's in BaseFrontendMixin and QtKernelClientMixin (base_frontend_mixin.py)
+    # TODO: try to use this code in myqtconsoleapp
 
     jupyter_widget = RichJupyterWidget()
     jupyter_widget.kernel_client = kernel_client
