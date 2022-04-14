9bf69f3ae8e44d91053b29a36cf2a3e43b232849
Justine Fricou <justine.fricou@gmail.com>
2022-04-14 14:17:01 +0200

    Fixing the client's crash

--- src_python/sot_ipython_connection/ipython_client/ipython_client.py
+++ src_python/sot_ipython_connection/ipython_client/ipython_client.py
@@ -10,9 +10,6 @@ from qtconsole.client import QtKernelClient
 
 nest_asyncio.apply()
 
-def shutdown_kernel():
-    ...
-
 class SOTClient(QtKernelClient):
     def __init__(self, kernel_number):
         self.load_connection_file(jupyter_core.paths.jupyter_runtime_dir() +
@@ -22,26 +19,19 @@ class SOTClient(QtKernelClient):
     def run_python_command(self, cmd):
         self.execute(cmd, silent=False)
 
-    """ def __del__(self):
-        self.stop_channels() """
+    def __del__(self):
+        self.stop_channels()
 
 
-def main(kernel_number, cmd):
+def main(kernel_number, cmd1, cmd2):
     app = QtWidgets.QApplication.instance() 
     if not app:
         app = QtWidgets.QApplication(["test"])
 
     kernel_client = SOTClient(kernel_number)
-
-    jupyter_widget = RichJupyterWidget()
-    jupyter_widget.kernel_client = kernel_client
-
-    app.aboutToQuit.connect(shutdown_kernel)
-    kernel_client.execute(cmd, silent=False)
-    jupyter_widget.kernel_manager.shutdown_kernel()
-
-    sys.exit(app.exec_())
+    kernel_client.execute(cmd1, silent=False)
+    kernel_client.execute(cmd2, silent=False)
 
 if __name__ == "__main__":
-    assert len(sys.argv) == 3
-    main(sys.argv[1], sys.argv[2])
+    assert len(sys.argv) == 4
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
