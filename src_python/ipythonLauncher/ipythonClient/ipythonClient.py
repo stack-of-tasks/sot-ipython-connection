8c2fb74f81c7b4c681c381033cc299990db912c2
Justine Fricou <justine.fricou@gmail.com>
2022-04-14 13:28:30 +0200

    Semi-fix of the client: it now send the python code properly but then crashes

--- src_python/ipythonLauncher/ipythonClient/ipythonClient.py
+++ src_python/ipythonLauncher/ipythonClient/ipythonClient.py
@@ -1,31 +1,47 @@
 
 import sys
-from qtconsole.client import QtKernelClient
-import jupyter_core
+
 import nest_asyncio
+import jupyter_core
 
+from PyQt5 import QtWidgets
+from qtconsole.rich_jupyter_widget import RichJupyterWidget
+from qtconsole.client import QtKernelClient
 
-class KernelClient:
+nest_asyncio.apply()
+
+def shutdown_kernel():
+    ...
+
+class SOTClient(QtKernelClient):
     def __init__(self, kernel_number):
-        self.kernel_client = QtKernelClient()
-        if len(sys.argv) == 1:
-            exit()
-        self.kernel_client.load_connection_file(jupyter_core.paths.jupyter_runtime_dir() +
+        self.load_connection_file(jupyter_core.paths.jupyter_runtime_dir() +
             '/kernel-' + kernel_number + '.json')
-        self.kernel_client.start_channels()
+        self.start_channels()
 
     def run_python_command(self, cmd):
-        self.kernel_client.execute(cmd, silent=False)
+        self.execute(cmd, silent=False)
 
-    #def __del__(self):
-        # self.kernel_client.stop_channels()
+    """ def __del__(self):
+        self.stop_channels() """
 
 
 def main(kernel_number, cmd):
-    client = KernelClient(kernel_number)
-    client.run_python_command(cmd)
+    app = QtWidgets.QApplication.instance() 
+    if not app:
+        app = QtWidgets.QApplication(["test"])
+
+    kernel_client = SOTClient(kernel_number)
+
+    jupyter_widget = RichJupyterWidget()
+    jupyter_widget.kernel_client = kernel_client
+
+    app.aboutToQuit.connect(shutdown_kernel)
+    kernel_client.execute(cmd, silent=False)
+    jupyter_widget.kernel_manager.shutdown_kernel()
+
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     assert len(sys.argv) == 3
-    nest_asyncio.apply()
     main(sys.argv[1], sys.argv[2])
