842f4d54ee523720d4529245aca3c86f88fa1563
Justine Fricou <justine.fricou@gmail.com>
2022-04-15 10:02:47 +0200

    Init sot_graphical_client.py

--- src_python/sot_ipython_connection/app/sot_graphical_client.py
+++ src_python/sot_ipython_connection/app/sot_graphical_client.py
@@ -1,2 +1,16 @@
-# TODO
-# main launching the qt client
+import sys
+import os
+
+from PyQt5 import QtWidgets
+
+scriptDirectory = os.path.dirname(__file__)
+moduleDirectory = os.path.join(scriptDirectory, '..')
+sys.path.append(moduleDirectory)
+from sot_client import SOTClient
+
+def main(scripts_paths):
+    ... # TODO: launch the qt client
+
+
+if __name__ == "__main__":
+    main()
