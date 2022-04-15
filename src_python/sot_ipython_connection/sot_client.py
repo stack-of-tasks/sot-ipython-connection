d6cea6ed61326a32b4755cd8293d8a59ea7a675a
Justine Fricou <justine.fricou@gmail.com>
2022-04-15 15:55:01 +0200

    Adding the structure of the future functions + comments for the SOTClient class

--- src_python/sot_ipython_connection/sot_client.py
+++ src_python/sot_ipython_connection/sot_client.py
@@ -13,8 +13,16 @@ def get_latest_connection_file_path():
     files = directory_path.glob("*")
     return max(files, key=lambda x: x.stat().st_ctime)
 
+""" Documentation on messaging with jupyter:
+    https://jupyter-client.readthedocs.io/en/latest/messaging.html#messaging-in-jupyter
 
-class SOTClientOut:
+    Documentation on BlockingKernelClient:
+    https://www.adamsmith.haus/python/docs/jupyter_client.BlockingKernelClient
+"""
+
+
+class SOTCommandInfo:
+    session = None # To know which client has sent the command
     cmd = None
     result = None
     stdout = None
@@ -23,13 +31,42 @@ class SOTClientOut:
 
 class SOTClient(BlockingKernelClient):
     def __init__(self):
+        # List of SOTCommandInfo: history of every received response
+        # (triggered by a command from this client or another)
+        self.cmd_history = []
+
+        # Setting up and starting the communication with the kernel
         self.load_connection_file(get_latest_connection_file_path())
         self.start_channels()
 
+
+    def is_response_to_self(self, response):
+        """ Is the given response responding to a request sent by this
+            client ?
+        """
+        return True
+        # TODO: see if the session's id can be retreived
+        # and compare it to the one in the parent_header
+
+
+    def get_self_history(self):
+        """ Returns a filtered copy of self.cmd_history by keeping
+            only the commands that were sent by this client
+        """
+        ... # TODO -> use self.is_response_to_self()
+
+
+    def save_command_info(self, response):
+        ...
+
+
     def run_python_command(self, cmd):
-        # Documentation on messaging with jupyter:
-        # https://jupyter-client.readthedocs.io/en/latest/messaging.html#messaging-in-jupyter
-        
+        """ This is a blocking function that sends a command to the
+            kernel and waits for it to respond completely (see link
+            to the doc on messaging with jupyter for the steps),
+            while saving every response it gets to self.cmd_history
+        """
+
         # Sending the command to the kernel
         msg_id = self.execute(cmd)
 
@@ -39,8 +76,14 @@ class SOTClient(BlockingKernelClient):
                 # iopub is the channel where the kernel broadcasts side-effects
                 # (stderr, stdout, debugging events, its status: busy or idle, etc)
                 response = self.get_iopub_msg()
-                print(response)
-                print()
+                
+                # Printing the response if it's responding to OUR command
+                if self.is_response_to_self(response):
+                    print(response)
+                    print()
+
+                # Saving the response to the command history
+                self.save_command_info(response)
 
                 # We can stop listening to the kernel's responses if it
                 # changes its status to 'idle' in response to OUR command.
@@ -51,12 +94,11 @@ class SOTClient(BlockingKernelClient):
                     whole_response_received = True
             except:
                 ...
-        
-        # TODO: return un SOTClientOut, le save dans une liste pour faire
-        # un historique
+
 
     def run_python_script(self, filepath):
         self.run_python_command("%run " + str(filepath))
+
 
     def __del__(self):
         self.stop_channels()
