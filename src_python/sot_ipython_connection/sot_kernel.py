645db9ae0ec102701fa5cd20613de5a1f4cdeecc
Justine Fricou <justine.fricou@gmail.com>
2022-04-15 09:16:22 +0200

    sot_kernel now uses a SOTKernel class

--- src_python/sot_ipython_connection/sot_kernel.py
+++ src_python/sot_ipython_connection/sot_kernel.py
@@ -5,40 +5,41 @@ from ipykernel.kernelapp import launch_new_instance
 
 class SOTKernel:
     def __init__(self):
-        ...
+        # TODO: use Path
+        # Importing the connection and namespace configurations
+        scriptDirectory = os.path.dirname(__file__)
+        moduleDirectory = os.path.join(scriptDirectory, '..')
+        sys.path.append(moduleDirectory)
+        import connection_config
+        import kernel_namespace_config
+
+        # The kernel's connection info (ports, ip...)
+        self.connectionConfig = connection_config.config
+
+        # The kernel's user namespace will be initialized with the
+        # variables contained in this dictionary:
+        self.namespace = kernel_namespace_config.kernel_namespace
+
+    def run(self):
+        # List of the kernel's options:
+        # https://ipython.readthedocs.io/en/7.23.0/config/options/kernel.html
+        launch_new_instance(
+            user_ns = self.namespace,
+            shell_port = self.connectionConfig["shell_port"],
+            iopub_port = self.connectionConfig["iopub_port"],
+            stdin_port = self.connectionConfig["stdin_port"],
+            control_port = self.connectionConfig["control_port"],
+            hb_port = self.connectionConfig["hb_port"],
+            ip = self.connectionConfig["ip"],
+            transport = self.connectionConfig["transport"],
+            signature_scheme = self.connectionConfig["signature_scheme"]
+        )
 
 
 def main():
+    kernel = SOTKernel()
+    kernel.run()
     
-    # TODO: use Path
-    # Importing the connection and namespace configurations
-    scriptDirectory = os.path.dirname(__file__)
-    moduleDirectory = os.path.join(scriptDirectory, '..')
-    sys.path.append(moduleDirectory)
-    import connection_config
-    import kernel_namespace_config
-
-    # The kernel's connection info (ports, ip...)
-    connectionConfig = connection_config.config
-
-    # The kernel's user namespace will be initialized with the
-    # variables contained in this dictionary:
-    namespace = kernel_namespace_config.kernel_namespace
-
-    # List of the kernel's options:
-    # https://ipython.readthedocs.io/en/7.23.0/config/options/kernel.html
-    launch_new_instance(
-        user_ns = namespace,
-        shell_port = connectionConfig["shell_port"],
-        iopub_port = connectionConfig["iopub_port"],
-        stdin_port = connectionConfig["stdin_port"],
-        control_port = connectionConfig["control_port"],
-        hb_port = connectionConfig["hb_port"],
-        ip = connectionConfig["ip"],
-        transport = connectionConfig["transport"],
-        signature_scheme = connectionConfig["signature_scheme"]
-    )
-
 
 if __name__ == "__main__" :
     main()
