73296afbde53ef468ceadacb6b275c06e79349e3
Justine Fricou <justine.fricou@gmail.com>
2022-04-19 17:33:38 +0200

    Starting to implement tests for the script executer

--- tests/test_script_executer.py
+++ tests/test_script_executer.py
@@ -1,13 +1,41 @@
-class TestScriptExecuter:
+import os
+import sys
+import unittest
+
+from PyQt5 import QtWidgets
+
+scriptDirectory = os.path.dirname(__file__)
+moduleDirectory = os.path.join(scriptDirectory, '..')
+sys.path.append(moduleDirectory)
+from src_python.sot_ipython_connection.sot_client import SOTClient
+from src_python.sot_ipython_connection.app.sot_script_executer import main as script_executer
+
+class TestScriptExecuter(unittest.TestCase):
     """ These tests must be run after launching a new kernel
     """
 
-    def test_no_script(self):
-        ... # TODO
+    @classmethod
+    def setUpClass(self):
+        ...
+        """ print(scriptDirectory + "/python_script_1.py")
+        app = QtWidgets.QApplication.instance()
+        if app is None:
+            app = QtWidgets.QApplication([])
+
+        self.kernel_client = SOTClient() """
 
 
     def test_var_definition(self):
-        ... # TODO
+        ...
+        """ script_executer(scriptDirectory + "/python_script_1.py")
+        self.kernel_client.run_python_command("script_var_1")
+        self.kernel_client.run_python_command("script_var_2")
+
+        assert len(self.kernel_client.cmd_history) == 2
+        assert self.kernel_client.cmd_history[0].stdout == "1"
+        assert self.kernel_client.cmd_history[0].stderr == None
+        assert self.kernel_client.cmd_history[1].stdout == "2"
+        assert self.kernel_client.cmd_history[1].stderr == None """
         
 
     def test_var_redefinition(self):
