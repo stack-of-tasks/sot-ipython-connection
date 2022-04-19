51228e1e73e28f2d611bec0be868f20bd013894f
Justine Fricou <justine.fricou@gmail.com>
2022-04-19 17:14:20 +0200

    Implementing tests for the command history feature

--- tests/test_client_history.py
+++ tests/test_client_history.py
@@ -1,21 +1,84 @@
+import os
+import sys
+
+from PyQt5 import QtWidgets
+
+scriptDirectory = os.path.dirname(__file__)
+moduleDirectory = os.path.join(scriptDirectory, '..')
+sys.path.append(moduleDirectory)
+from src_python.sot_ipython_connection.sot_client import SOTClient
+
 class TestClientHistory:
     """ These tests must be run after launching a new kernel
     """
 
     def test_no_cmd(self):
-        ... # TODO
+        app = QtWidgets.QApplication.instance() 
+        if not app:
+            app = QtWidgets.QApplication([])
+
+        kernel_client = SOTClient()
+        assert len(kernel_client.cmd_history) == 0
 
 
     def test_one_cmd_successful(self):
-        ... # TODO
+        app = QtWidgets.QApplication.instance() 
+        if not app:
+            app = QtWidgets.QApplication([])
+
+        kernel_client = SOTClient()
+        kernel_client.run_python_command("1 + 1")
+
+        assert len(kernel_client.cmd_history) == 1
+
+        assert kernel_client.cmd_history[0].session_id != None
+        assert kernel_client.cmd_history[0].id != None
+        assert kernel_client.cmd_history[0].content == "1 + 1"
+        assert kernel_client.cmd_history[0].stdout == "2"
+        assert kernel_client.cmd_history[0].stderr == None
 
 
     def test_one_cmd_error(self):
-        ... # TODO
+        app = QtWidgets.QApplication.instance() 
+        if not app:
+            app = QtWidgets.QApplication([])
+
+        kernel_client = SOTClient()
+        kernel_client.run_python_command("unknown_variable")
+
+        assert len(kernel_client.cmd_history) == 1
+
+        assert kernel_client.cmd_history[0].session_id != None
+        assert kernel_client.cmd_history[0].id != None
+        assert kernel_client.cmd_history[0].content == "unknown_variable"
+        assert kernel_client.cmd_history[0].stdout == None
+        assert kernel_client.cmd_history[0].stderr != None
         
 
     def test_several_cmd(self):
-        ... # TODO
+        app = QtWidgets.QApplication.instance() 
+        if not app:
+            app = QtWidgets.QApplication([])
+
+        kernel_client = SOTClient()
+        kernel_client.run_python_command("test_history_1 = 1")
+        kernel_client.run_python_command("test_history_1")
+
+        assert len(kernel_client.cmd_history) == 2
+
+        assert kernel_client.cmd_history[0].session_id == kernel_client.session_id
+        assert kernel_client.cmd_history[0].id != None
+        assert kernel_client.cmd_history[0].content == "test_history_1 = 1"
+        assert kernel_client.cmd_history[0].stdout == None
+        assert kernel_client.cmd_history[0].stderr == None
+
+        assert kernel_client.cmd_history[1].session_id == kernel_client.session_id
+        assert kernel_client.cmd_history[1].id != None
+        assert kernel_client.cmd_history[1].content == "test_history_1"
+        assert kernel_client.cmd_history[1].stdout == "1"
+        assert kernel_client.cmd_history[1].stderr == None
+
+        assert kernel_client.cmd_history[1].id != kernel_client.cmd_history[0].id
         
 
     def test_several_sessions(self):
