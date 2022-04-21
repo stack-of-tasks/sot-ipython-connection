6e9c1e7ff6dc92974baf74bec3e5c9dbbe3452fa
Justine Fricou <justine.fricou@gmail.com>
2022-04-21 14:39:51 +0200

    Adding a very basic sot_logs_client that prints all messages broadcasted by the kernel

---
+++ src_python/sot_ipython_connection/app/sot_logs_client.py
@@ -0,0 +1,42 @@
+import sys
+import os
+
+from PyQt5 import QtWidgets
+
+scriptDirectory = os.path.dirname(__file__)
+moduleDirectory = os.path.join(scriptDirectory, '..')
+sys.path.append(moduleDirectory)
+from sot_client import SOTClient
+
+
+def main():
+    app = QtWidgets.QApplication.instance() 
+    if not app:
+        app = QtWidgets.QApplication([])
+
+    kernel_client = SOTClient()
+
+    exit_requested = False
+
+    while not exit_requested:
+        #user_input = input()
+        #if user_input in ["exit", "quit", "exit()", "quit()"]:
+        #    exit_requested = True
+
+        try:
+            print("try")
+            # iopub is the channel where the kernel broadcasts side-effects
+            # (stderr, stdout, debugging events, its status: busy or idle, etc)
+            # to every client connected to it
+            response = kernel_client.get_iopub_msg()
+            print(response)
+
+            # Saving the response to the command history
+            kernel_client.save_command_info(response)
+
+        except: # Entered when there is reponse to read
+            ...
+
+
+if __name__ == "__main__":
+    main()
